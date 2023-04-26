c62336d9719a1af8a37ef3e8ebd1c761c318d992
Mathis Petrovich <mathis.petrovich@enpc.fr>
2023-04-26 04:29:54 +0200

    gradio js test

--- tmr/gradio.js
+++ tmr/gradio.js
@@ -9,4 +9,4 @@ function make_script(src) {
     document.head.appendChild(script);
 }
 // https://gradio.s3-us-west-2.amazonaws.com/3.24.1/assets/index.18ea9dc6.js
-make_script("index.18ea9dc6.js");
+make_script("https://mathis.petrovich.fr/tmr/index.18ea9dc6.js");
