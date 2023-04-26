0e144a98254deff0d860f189ca925a60a382fbfd
Mathis Petrovich <mathis.petrovich@enpc.fr>
2023-04-26 04:24:56 +0200

    add gradio js

---
+++ tmr/gradio.js
@@ -0,0 +1,12 @@
+// the script was from
+// https://gradio.s3-us-west-2.amazonaws.com/3.24.1/gradio.js
+
+function make_script(src) {
+    const script = document.createElement('script');
+    script.type = 'module';
+    script.setAttribute("crossorigin", "");
+    script.src = src;
+    document.head.appendChild(script);
+}
+// https://gradio.s3-us-west-2.amazonaws.com/3.24.1/assets/index.18ea9dc6.js
+make_script("index.18ea9dc6.js");
